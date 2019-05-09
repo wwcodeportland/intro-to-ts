531095d9a0793667d1172791f6861555379610ba
VerteDinde <keeleyhammond@invisionapp.com>
2019-05-08 17:44:38 -0700

    Add comments to index file

--- compiling-demo/src/index.ts
+++ compiling-demo/src/index.ts
@@ -1,12 +1,15 @@
+// Add our own timeout
 function timeout(n: number) {
   return new Promise(res => setTimeout(res, n));
 }
 
+// Greeting function, takes two strings and a number
 export async function add(greeting: string, a: number, animal: string) {
   await timeout(500);
   return greeting + a + animal;
 }
 
+// Output
 (async () => {
   console.log(await add('Joan counted ', 3, ' penguins.'));
 })();
